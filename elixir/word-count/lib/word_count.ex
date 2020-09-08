64870e61514379096faabecd7bdde24ab2565b3d
cafemug <hyanghope@gmail.com>
2020-09-08 17:21:52 +0900

    3. word count solved

---
+++ elixir/word-count/lib/word_count.ex
@@ -0,0 +1,20 @@
+defmodule WordCount do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t()) :: map
+  def count(sentence) do
+    sentence
+    |> String.split()
+    |> word_count(%{})
+  end
+
+  @spec word_count(List.t(), Map.t()) :: map
+  def word_count(word_list, c_map) do
+    c_map = Enum.reduce(word_list, %{}, fn (s, map) ->  Map.update(map, s, 1, &(&1 + 1)) end)
+    c_map
+    end
+  
+end
