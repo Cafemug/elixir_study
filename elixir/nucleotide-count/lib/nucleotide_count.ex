5aa5af665096546148ddd36eee6e8d94abab977e
cafemug <hyanghope@gmail.com>
2020-09-09 10:47:28 +0900

    7. nucleotide-count solved

---
+++ elixir/nucleotide-count/lib/nucleotide_count.ex
@@ -0,0 +1,34 @@
+defmodule NucleotideCount do
+  @nucleotides [?A, ?C, ?G, ?T]
+
+  @doc """
+  Counts individual nucleotides in a DNA strand.
+
+  ## Examples
+
+  iex> NucleotideCount.count('AATAA', ?A)
+  4
+
+  iex> NucleotideCount.count('AATAA', ?T)
+  1
+  """
+  @spec count(charlist(), char()) :: non_neg_integer()
+  def count(strand, nucleotide) do
+    c_map = histogram(strand)
+    Map.get(c_map, nucleotide, 0)
+  end
+
+  @doc """
+  Returns a summary of counts by nucleotide.
+
+  ## Examples
+
+  iex> NucleotideCount.histogram('AATAA')
+  %{?A => 4, ?T => 1, ?C => 0, ?G => 0}
+  """
+  @spec histogram(charlist()) :: map()
+  def histogram(strand) do
+    c_map = Enum.reduce(strand, %{}, fn (s, map) ->  Map.update(map, s, 1, &(&1 + 1)) end)
+    c_map
+  end
+end
