313022a5472b0a9d726ef2664b06061228e95d2e
cafemug <hyanghope@gmail.com>
2020-09-10 17:44:36 +0900

    18. protein translation  solved

---
+++ elixir/protein-translation/lib/protein_translation.ex
@@ -0,0 +1,64 @@
+defmodule ProteinTranslation do
+  defp remove_stop(str_list) do
+    str_list
+    |> Enum.reduce_while([], fn {_, x} , result -> if x != "STOP", do: {:cont, result ++ [x]}, else: {:halt, result} end)
+  end
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {atom, list(String.t())}
+  def of_rna(rna) do
+    result = 
+    rna
+    |> to_charlist()
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn x -> of_codon(to_string(x)) end)
+    |> Enum.uniq()
+    if {:error, "invalid codon"} in result, do: {:error, "invalid RNA"}, else: {:ok, remove_stop(result)} 
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {atom, String.t()}
+  def of_codon(codon) do
+    case codon do
+      "UGU" -> {:ok, "Cysteine"}
+      "UGC" -> {:ok, "Cysteine"}
+      "UUA" -> {:ok, "Leucine"}
+      "UUG" -> {:ok, "Leucine"}
+      "AUG" -> {:ok, "Methionine"}
+      "UUU" -> {:ok, "Phenylalanine"}
+      "UUC" -> {:ok, "Phenylalanine"}
+      "UCU" -> {:ok, "Serine"}
+      "UCC" -> {:ok, "Serine"}
+      "UCA" -> {:ok, "Serine"}
+      "UCG" -> {:ok, "Serine"}
+      "UGG" -> {:ok, "Tryptophan"}
+      "UAU" -> {:ok, "Tyrosine"}
+      "UAC" -> {:ok, "Tyrosine"}
+      "UAA" -> {:ok, "STOP"}
+      "UAG" -> {:ok, "STOP"}
+      "UGA" -> {:ok, "STOP"}
+      _ -> {:error, "invalid codon"}
+    end
+  end
+end
