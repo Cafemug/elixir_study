ce35c86dc9d0a8b2950184353a7ad8c483ebed0d
cafemug <hyanghope@gmail.com>
2020-09-11 17:41:17 +0900

    19. grade school  solved

---
+++ elixir/grade-school/lib/school.ex
@@ -0,0 +1,40 @@
+defmodule School do
+  @moduledoc """
+  Simulate students in a school.
+
+  Each student is in a grade.
+  """ 
+  
+  @doc """
+  Add a student to a particular grade in school.
+  """
+  @spec add(map, String.t(), integer) :: map
+  def add(db, name, grade) do
+    db = 
+    case Map.fetch(db, grade) do
+      {:ok, _} -> Map.put(db, grade, db[grade] ++ [name])
+      _ -> Map.put(db, grade, [name])
+    end
+    db
+  end
+
+  @doc """
+  Return the names of the students in a particular grade.
+  """
+  @spec grade(map, integer) :: [String.t()]
+  def grade(db, grade) do
+    Map.get(db, grade, [])
+  end
+
+  @doc """
+  Sorts the school by grade and name.
+  """
+  @spec sort(map) :: [{integer, [String.t()]}]
+  def sort(db) do
+    db
+    |> Enum.reduce(%{}, fn ({k,v}, result) -> Map.put(result, k, Enum.sort(v)) end)
+    |> Enum.reduce([], fn ({k,v}, result) -> result ++ [{k,v}] end)
+  end
+
+
+end
