c6ff97f308163e130bd45a24670664f4754b93e1
cafemug <hyanghope@gmail.com>
2020-09-14 10:42:15 +0900

    feat():simply  func plug add

--- project/lib/project_web/endpoint.ex
+++ project/lib/project_web/endpoint.ex
@@ -51,4 +51,14 @@ defmodule ProjectWeb.Endpoint do
   plug Plug.Head
   plug Plug.Session, @session_options
   plug ProjectWeb.Router
+  plug :introspect
+  def introspect(conn, _opts) do
+    IO.puts """
+    Verb: #{inspect(conn.method)}
+    Host: #{inspect(conn.host)}
+    Headers: #{inspect(conn.req_headers)}
+    """
+
+    conn
+  end
 end
