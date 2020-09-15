4bb329115057da68c8fb2ff7b17661aae788bb1d
cafemug <hyanghope@gmail.com>
2020-09-15 11:37:36 +0900

    fix(): session add

--- project/lib/project_web/router.ex
+++ project/lib/project_web/router.ex
@@ -19,6 +19,8 @@ defmodule ProjectWeb.Router do
 
     get "/", PageController, :index
     resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete],
+                                              singleton: true
     get "/redirect_test", PageController, :redirect_test
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
