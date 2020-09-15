c32df4256f6409f418708ea99f51e5e857f1d2a5
cafemug <hyanghope@gmail.com>
2020-09-15 11:29:23 +0900

    fix():account add

--- project/lib/project_web/router.ex
+++ project/lib/project_web/router.ex
@@ -18,6 +18,7 @@ defmodule ProjectWeb.Router do
     pipe_through :browser
 
     get "/", PageController, :index
+    resources "/users", UserController
     get "/redirect_test", PageController, :redirect_test
     get "/hello", HelloController, :index
     get "/hello/:messenger", HelloController, :show
